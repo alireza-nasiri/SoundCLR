386918af4f3f9ddc1e69d21b52bfcda025836981
Alireza Nasiri <alireza.nasiry@gmail.com>
2021-02-22 14:14:04 -0500

    Update dataset_ESC10.py

--- dataset_ESC10.py
+++ dataset_ESC10.py
@@ -26,11 +26,11 @@ device = torch.device("cuda" if use_cuda else "cpu")
 class MyDataset(data.Dataset):
     
     def __init__(self, train=True):
-        self.root = '/work/anasiri/CPC_audio_spectrograms/data/waves_ESC50/'
+        self.root = '/data/ESC50/waves_ESC50/'
         self.train = train
         
         #getting name of all files inside the all of the train_folds
-        temp = open('data/ESC10_file_names.txt', 'r').read().split('\n')
+        temp = open('data/ESC50/ESC10_file_names.txt', 'r').read().split('\n')
         temp.sort()
         self.file_names = []
         if train:
@@ -63,23 +63,24 @@ class MyDataset(data.Dataset):
 
     
     def __len__(self):
-        return len(self.file_names) # * 2
+        return len(self.file_names)
     
     
 
     def __getitem__(self, index):
-        file_name = self.file_names[index ]  #  % len(self.file_names)
+        file_name = self.file_names[index ]  
         path = self.root + file_name
         wave, rate = librosa.load(path, sr=44100)
         
-        
+        #identifying the label of the sample from its name
         temp = file_name.split('.')[0]
         class_id = int(temp.split('-')[-1])
         class_id = config.ESC10_classIds.index(class_id)
         
         if wave.ndim == 1:
             wave = wave[:, np.newaxis]
-
+        
+        # normalization of wave to [-1, 1]
         if np.abs(wave.max()) > 1.0:
             wave = transforms.scale(wave, wave.min(), wave.max(), -1.0, 1.0)
         wave = wave.T * 32768.0
@@ -96,35 +97,18 @@ class MyDataset(data.Dataset):
         s = librosa.feature.melspectrogram(wave_copy.numpy(), sr=44100, n_mels=128, n_fft=1024, hop_length=512) 
         log_s = librosa.power_to_db(s, ref=np.max)
         
+        # random-masking the spectrograms
         log_s = self.spec_transforms(log_s)
         
-        '''
-        spec = torch.empty(0, 128, 431)
-        for i in range(3):
-            
-            log_s_augmented = self.spec_transforms(log_s)
-            
-            spec = torch.cat((spec, log_s_augmented), dim=0)
-        '''
-        
-        #creating 3 channels by copying log_s1 3 times 
+        # making the input spectrogram to have 3 identical channels
         spec = torch.cat((log_s, log_s, log_s), dim=0)
         
-        
-        
-        #if not self.train: # for the test loader
         return file_name, spec, class_id
         
     
     
     
     
-    
-    
-    
-    
-    
-
 def create_generators():
     
     train_dataset = MyDataset(train=True)
@@ -137,43 +121,4 @@ def create_generators():
     return train_loader, test_loader
     
     
-    '''
-    overall_dataset = MyDataset(train=True)
-    
-    
-    
-    train_dataset, val_dataset = data.random_split(overall_dataset, [int(len(overall_dataset)*0.8), 
-                                                                         len(overall_dataset)-int(len(overall_dataset)*0.8)])
-    
-    train_loader = data.DataLoader(train_dataset, batch_size = config.batch_size, shuffle=True, num_workers=1,drop_last=True)
-    
-    val_loader = data.DataLoader(val_dataset, batch_size = config.batch_size, shuffle=True, num_workers = 1,drop_last=True)
-    
-    return train_loader, val_loader
-    
-    '''
-
-    '''
-    # Creating data indices for training and validation splits:
-    dataset_size = len(overall_dataset)
-    indices = list(range(dataset_size))
-    split = int(np.floor(0.15 * dataset_size))
-    
-    np.random.seed(random_seed)
-    np.random.shuffle(indices)
-    train_indices, val_indices = indices[split:], indices[:split]
-
-    # Creating PT data samplers and loaders:
-    train_sampler = SubsetRandomSampler(train_indices)
-    valid_sampler = SubsetRandomSampler(val_indices)
-
-    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler)
-    validation_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=valid_sampler)
-    return train_loader, validation_loader
-    '''
-
-
-
-
-
-
+   
