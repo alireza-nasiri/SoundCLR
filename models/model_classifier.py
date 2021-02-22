f2cbd88898c21d26d7fcb1ed39b434c1cd82fbe1
Alireza Nasiri <alireza.nasiry@gmail.com>
2021-02-22 14:03:23 -0500

    Update model_classifier.py

--- models/model_classifier.py
+++ models/model_classifier.py
@@ -6,22 +6,16 @@ import config
 
 
 
-# CUDA for PyTorch
-#use_cuda = torch.cuda.is_available()
-#device = torch.device("cuda:2" if use_cuda else "cpu")
-
-
 class Classifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(2048, config.class_numbers)
+        self.fc = nn.Linear(2048, config.class_numbers)
         
         
         
         
     def forward(self, x):
-        x = self.fc1(x)
-        
+        x = self.fc(x)
         
         return x
     
