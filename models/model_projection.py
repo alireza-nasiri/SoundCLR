8c6e9411f8cca36fffbb3c4b14b7d162734daef1
Alireza Nasiri <alireza.nasiry@gmail.com>
2021-02-22 14:03:59 -0500

    Update model_projection.py

--- models/model_projection.py
+++ models/model_projection.py
@@ -6,20 +6,15 @@ import config
 
 
 
-# CUDA for PyTorch
-#use_cuda = torch.cuda.is_available()
-#device = torch.device("cuda:2" if use_cuda else "cpu")
-
-
 class ProjectionModel(nn.Module):
 	def __init__(self):
 		super().__init__()
 		self.fc = nn.Linear(2048, 64)
-		#self.fc = nn.Linear(1920, 64)
         
         
 	def forward(self, x):
 		x = self.fc(x)
+		
 		return x
     
     
